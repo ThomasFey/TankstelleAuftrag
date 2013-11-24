a8c94fc0a49a227b6ac9746ded2a673d0d9b9407
PinguJr <yannik108@hotmail.ch>
2013-11-24 13:13:48 +0100

    Methode displayInfo

--- Tankstelle_Projekt/src/AnzeigeSchild.java
+++ Tankstelle_Projekt/src/AnzeigeSchild.java
@@ -1,10 +1,12 @@
-
 import java.util.List;
 
 public class AnzeigeSchild {
 	
 	public void displayInfo (List<Treibstoff> pTreibstoff)
 	{
-		
+		for(Treibstoff einTreibstoff : pTreibstoff)
+		{
+			System.out.println(einTreibstoff.getBezeichnung() +  "Preis pro Liter" + einTreibstoff.getPreis)
+		}
 	}
 }
