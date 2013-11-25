500d4a3a8b80405cfce41480b330e3a106c9a723
PinguJr <yannik108@hotmail.ch>
2013-11-25 20:37:13 +0100

    Add Treibstoffe

--- Tankstelle_Projekt/src/Tankstelle.java
+++ Tankstelle_Projekt/src/Tankstelle.java
@@ -1,10 +1,20 @@
+import java.util.List;
 
 public class Tankstelle {
 	private String mStandort;
+	private List<Treibstoff> mTreibstoffe;
 	
 	public Tankstelle()
 	{
-		
+		mTreibstoffe = new Arraylist<Treibstoff>();
+	}
+	public void addTreibstoff(Treibstoff pTreibstoff)
+	{
+		mTreibstoffe.Add(pTreibstoff);
+	}
+	public Treibstoff getTreibstoffeById(int pId)
+	{
+		return mTreibstoffe.FindByIndex(pId);
 	}
 
 	public void setmStandort(String pStandort) {
