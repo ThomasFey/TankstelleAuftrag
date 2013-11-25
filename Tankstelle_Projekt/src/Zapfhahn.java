d35ef75ea0572ca9feec2116fd171e6f0948ad44
PinguJr <yannik108@hotmail.ch>
2013-11-25 19:54:07 +0100

    Update Attribut

--- Tankstelle_Projekt/src/Zapfhahn.java
+++ Tankstelle_Projekt/src/Zapfhahn.java
@@ -4,17 +4,21 @@ import java.util.List;
 public class Zapfhahn {
 	private int mNummer;
 	private Treibstoff mTreibstoffe;
+	private Zapfsaeule mZapfsaeule;
 	
-	public Zapfhahn (Treibstoff pTreibstoffe, int pNummer)
+	public Zapfhahn (Treibstoff pTreibstoffe, int pNummer, Zapfsaeule pZapfsaeule)
 	{
 		mTreibstoffe = pTreibstoffe;
 		mNummer = pNummer;
+		mZapfsaeule = pZapfsaeule;
 	}
 
 	public int getmNummer() {
 		return mNummer;
 	}
-	
+	public Zapfsaeule getmZapfsaeule() {
+		return Zapfsaeule;
+	}
 	public Treibstoff getmTreibstoffe() {
 		return mTreibstoffe;
 	}
